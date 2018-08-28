48bb9e10c4753073eca454d31c9c106fe2199b71
unknown <goyalm680@gmail.com>
2018-08-28 21:41:20 +0530

    my assignments

--- staticblock.java
+++ staticblock.java
@@ -4,12 +4,18 @@ static int num;
 static String str;
 static
 {
-num=97;
-str="static keyword ";
+System.out.println("block 1");
+num=16;
+str="block1";
+}
+static{
+System.out.println("block 2");
+num=18;
+str="block2";
 }
 public static void main(String args[])
 {
-System.out.println("value of num is: "+num);
-System.out.println("value of string is: "+str);
+System.out.println("num " +num);
+System.out.println("string " +str);
 }
 }
